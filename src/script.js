22c3c9499bde9cde99e279ba20456501760abd3c
MadFlip <mykola@bleech.de>
2022-09-11 20:41:01 +0200

    feat(script): play with material properties (map, transparent, opacity, side, alfa)

--- src/script.js
+++ src/script.js
@@ -1,104 +1,58 @@
 import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 
 // Textures
-const loadingManager = new THREE.LoadingManager()
-const textureLoader = new THREE.TextureLoader(loadingManager)
-// const colorTexture = textureLoader.load('textures/door/color.jpg')
-const colorTexture = textureLoader.load('textures/minecraft.png')
+const textureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load('textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
 
-// colorTexture.repeat.x = 2
-// colorTexture.repeat.y = 3
-// colorTexture.wrapS = THREE.MirroredRepeatWrapping
-// colorTexture.wrapT = THREE.RepeatWrapping
-
-// colorTexture.offset.x = 0.5
-// colorTexture.offset.y = 0.5
-
-// colorTexture.rotation = Math.PI * 0.25
-// colorTexture.center.x = 0.5
-// colorTexture.center.y = 0.5
-
-colorTexture.generateMipmaps = false
-colorTexture.minFilter = THREE.NearestFilter
-colorTexture.magFilter = THREE.NearestFilter
-
-// Debug
-const gui = new dat.GUI({ closed: true, width: 400 })
+doorColorTexture.generateMipmaps = false
+doorColorTexture.minFilter = THREE.NearestFilter
+doorColorTexture.magFilter = THREE.NearestFilter
 
 // Scene
 const canvas = document.querySelector('.webgl')
 const scene = new THREE.Scene()
 
-// Group
-const group = new THREE.Group()
-scene.add(group)
+// Objects
+const material = new THREE.MeshBasicMaterial()
+material.map = doorColorTexture
+material.transparent = true
+// material.alphaMap = doorAlphaTexture
+// material.wireframe = true
+material.opacity = 0.5
+material.side = THREE.DoubleSide
 
-const parameters = { 
-    color1: '#ffffff', // #ff6347
-    color2: '#00bfff',
-    color3: '#ffdead',
-    spin: () => {
-        gsap.to(group.rotation, { duration: 1, y: group.rotation.y + 10 })
-    }
-}
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5
 
-// Geometry & Material
-const geometryCube1 = new THREE.BoxBufferGeometry(.5, 1, .5)
-const materialCube1 =  new THREE.MeshBasicMaterial({ 
-    color: parameters.color1,
-    map: colorTexture
-})
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1, 1, 100, 100),
+    material
+)
 
-const geometryCube2 = new THREE.BoxBufferGeometry(.5, .8, .5)
-const materialCube2 =  new THREE.MeshBasicMaterial({ 
-    color: parameters.color2
-})
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+    material
+)
+torus.position.x = 1.5
 
-const geometryCube3 = new THREE.BoxBufferGeometry(.5, .6, .5)
-const materialCube3 =  new THREE.MeshBasicMaterial({ 
-    color: parameters.color3
-})
-
-// Mesh
-const cube1 = new THREE.Mesh(geometryCube1, materialCube1)
-cube1.position.x = -.6
-
-const cube2 = new THREE.Mesh(geometryCube2, materialCube2)
-cube2.position.y = -0.1
-
-const cube3 = new THREE.Mesh(geometryCube3, materialCube3)
-cube3.position.x = .6
-cube3.position.y = -.2
-
-group.add(cube1, cube2, cube3)
-
-// GUI Debug
-gui.add(group.position, 'x').min(-3).max(3).step(.01).name('positionX')
-gui.add(group.position, 'y').min(-3).max(3).step(.01).name('positionY')
-gui.add(cube1.material, 'wireframe').name('wireframe 1')
-gui.add(cube2.material, 'wireframe').name('wireframe 2')
-gui.add(cube3.material, 'wireframe').name('wireframe 3')
-
-gui.addColor(parameters, 'color1').onChange(() => {
-    cube1.material.color.set(parameters.color1)
-})
-
-gui.addColor(parameters, 'color2').onChange(() => {
-    cube2.material.color.set(parameters.color2)
-})
-
-gui.addColor(parameters, 'color3').onChange(() => {
-    cube3.material.color.set(parameters.color3)
-})
-gui.add(parameters, 'spin')
+scene.add(sphere, plane, torus)
 
 // Camera
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 100)
-camera.position.set(0, 0, 4)
+camera.position.set(0, 0, 5)
 scene.add(camera)
 
 // Controls
@@ -118,7 +72,18 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(window.innerWidth, window.innerHeight)
 
+const clock = new THREE.Clock()
+
 const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+    sphere.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+
     controls.update()
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
@@ -133,19 +98,19 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix()
 })
 
-// window.addEventListener('dblclick', () => {
-//     const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-//     if (!fullscreenElement) {
-//         if (canvas.requestFullscreen) {
-//             canvas.requestFullscreen()
-//         } else if (canvas.webkitRequestFullscreen) {
-//             canvas.webkitRequestFullscreen()
-//         }
-//     } else {
-//         if (document.exitFullscreen) {
-//             document.exitFullscreen()
-//         } else if (document.webkitExitFullscreen) {
-//             document.webkitExitFullscreen()
-//         }
-//     }
-// })
+window.addEventListener('dblclick', () => {
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen()
+        } else if (canvas.webkitRequestFullscreen) {
+            canvas.webkitRequestFullscreen()
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen()
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+        }
+    }
+})
