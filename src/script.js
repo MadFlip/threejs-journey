34b17c8c44359a1172191c4706a444a832621019
MadFlip <mykola@bleech.de>
2022-09-30 09:47:34 +0200

    feat(scripts.js): use only 1 defaultMaterial for all objects

--- src/script.js
+++ src/script.js
@@ -39,24 +39,23 @@ const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
 // Physics material
-const concreteMaterial = new CANNON.Material('concrete')
-const plasticMaterial = new CANNON.Material('plastic')
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-    concreteMaterial,
-    plasticMaterial,
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
     {
         friction: 0.1,
         restitution: 0.7
     }
 )
-world.addContactMaterial(concretePlasticContactMaterial)
+world.addContactMaterial(defaultContactMaterial)
 
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
     mass: 1,
     position: new CANNON.Vec3(0, 3, 0),
     shape: sphereShape,
-    material: plasticMaterial
+    material: defaultMaterial
 })
 world.addBody(sphereBody)
 
@@ -65,7 +64,7 @@ const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
 floorBody.mass = 0
 floorBody.addShape(floorShape)
-floorBody.material = concreteMaterial
+floorBody.material = defaultMaterial
 // Rotate plane in cannon.js uses quaternion
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5)
 world.addBody(floorBody)
