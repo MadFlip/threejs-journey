38dba879f5295aee6a7f065cc311e1bc6a1a48a3
MadFlip <mykola@bleech.de>
2022-10-03 20:57:42 +0200

    feat(scripts.js):  add reset button to clear the scene

--- src/script.js
+++ src/script.js
@@ -32,6 +32,20 @@ debugObject.createBox = () => {
 }
 gui.add(debugObject, 'createBox')
 
+debugObject.reset = () => {
+    for(const object of objectsToUpdate) {
+        // Remove body
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        // Remove mesh
+        scene.remove(object.mesh)
+    }
+
+    objectsToUpdate.splice(0, objectsToUpdate.length)
+}
+gui.add(debugObject, 'reset')
+
 /**
  * Base
  */
