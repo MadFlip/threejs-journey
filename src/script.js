f7834eadd6e8e939d0311fa07118be47e2969895
MadFlip <mykola@bleech.de>
2022-11-25 16:52:56 +0100

    feat(script): use shaders patterns

--- src/script.js
+++ src/script.js
@@ -18,48 +18,20 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const flagTexture = textureLoader.load('/textures/flag-ukraine.png')
-
-/**
  * Test mesh
  */
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
-const count = geometry.attributes.position.count
-const randoms = new Float32Array(count)
-
-for (let i = 0; i < count; i++)
-{
-    randoms[i] = Math.random()
-}
-
-geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
-
 // Material
 const material = new THREE.ShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
-    side: THREE.DoubleSide,
-    transparent: true,
-    uniforms:
-    {
-        uFrequency: { value: new THREE.Vector2(10.0, 5.0) },
-        uTime: { value: 0 },
-        uColor: { value: new THREE.Color('yellow') },
-        uTexture: { value: flagTexture }
-    }
+    side: THREE.DoubleSide
 })
-
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
-mesh.scale.y = 2 / 3
 scene.add(mesh)
 
 /**
@@ -101,7 +73,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -109,15 +81,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update material
-    material.uniforms.uTime.value = elapsedTime
-
     // Update controls
     controls.update()
 
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,22 +1,7 @@
-uniform vec2 uFrequency; // frequency of the wave
-uniform float uTime; // time
-
 varying vec2 vUv;
-varying float vElevation;
 
 void main()
 {
-  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  elevation += modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
-
-  modelPosition.z += elevation;
-
-  vec4 viewPosition = viewMatrix * modelPosition;
-  vec4 projectionPosition = projectionMatrix * viewPosition;
-
-  gl_Position = projectionPosition;
-  vUv = uv;
-  vElevation = elevation;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vUv = uv;
 }
