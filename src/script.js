e2beda98c0dbfa8457c8dd8f50da7d36c7ed3712
MadFlip <mykola@bleech.de>
2023-03-06 17:58:24 +0100

    feat(script): 33. Performance tips

--- src/script.js
+++ src/script.js
@@ -1,22 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'lil-gui'
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
-import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass'
-import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
-import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader'
-import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader'
-import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass'
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+import Stats from 'stats.js'
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js'
+
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -24,70 +13,10 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Loaders
+ * Textures
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
 const textureLoader = new THREE.TextureLoader()
-
-/**
- * Update all materials
- */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-        {
-            child.material.envMapIntensity = 2.5
-            child.material.needsUpdate = true
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
-}
-
-/**
- * Environment map
- */
-const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
-environmentMap.encoding = THREE.sRGBEncoding
-
-scene.background = environmentMap
-scene.environment = environmentMap
-
-/**
- * Models
- */
-gltfLoader.load(
-    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(2, 2, 2)
-        gltf.scene.rotation.y = Math.PI * 0.5
-        scene.add(gltf.scene)
-
-        updateAllMaterials()
-    }
-)
-
-/**
- * Lights
- */
-const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 3, - 2.25)
-scene.add(directionalLight)
+const displacementTexture = textureLoader.load('/textures/displacementMap.png')
 
 /**
  * Sizes
@@ -110,10 +39,6 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-    // Update composer
-    effectComposer.setSize(sizes.width, sizes.height)
-    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -121,7 +46,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
+camera.position.set(2, 2, 6)
 scene.add(camera)
 
 // Controls
@@ -133,160 +58,324 @@ controls.enableDamping = true
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
+    powerPreference: 'high-performance',
     antialias: true
 })
 renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-renderer.physicallyCorrectLights = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.toneMapping = THREE.ReinhardToneMapping
-renderer.toneMappingExposure = 1.5
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(window.devicePixelRatio)
 
 /**
- * Post processing
+ * Test meshes
  */
-const renderTarget = new THREE.WebGLRenderTarget(sizes.width, sizes.height, {
-    samples: renderer.getPixelRatio() === 1 ? 3 : 0
-})
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(2, 2, 2),
+    new THREE.MeshStandardMaterial()
+)
+cube.castShadow = true
+cube.receiveShadow = true
+cube.position.set(- 5, 0, 0)
+scene.add(cube)
 
-const effectComposer = new EffectComposer(renderer, renderTarget)
-effectComposer.setSize(sizes.width, sizes.height)
-effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(1, 0.4, 128, 32),
+    new THREE.MeshStandardMaterial()
+)
+torusKnot.castShadow = true
+torusKnot.receiveShadow = true
+scene.add(torusKnot)
 
-const renderPass = new RenderPass(scene, camera)
-effectComposer.addPass(renderPass)
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1, 32, 32),
+    new THREE.MeshStandardMaterial()
+)
+sphere.position.set(5, 0, 0)
+sphere.castShadow = true
+sphere.receiveShadow = true
+scene.add(sphere)
 
-const dotScreenPass = new DotScreenPass()
-dotScreenPass.enabled = false
-effectComposer.addPass(dotScreenPass)
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    new THREE.MeshStandardMaterial()
+)
+floor.position.set(0, - 2, 0)
+floor.rotation.x = - Math.PI * 0.5
+floor.castShadow = true
+floor.receiveShadow = true
+scene.add(floor)
 
-const glitchPass = new GlitchPass()
-glitchPass.enabled = false
-effectComposer.addPass(glitchPass)
-
-const rgbShiftPass = new ShaderPass(RGBShiftShader)
-rgbShiftPass.enabled = false
-effectComposer.addPass(rgbShiftPass)
-
-const unrealBloomPass = new UnrealBloomPass()
-unrealBloomPass.enabled = false
-unrealBloomPass.strength = 1.5
-unrealBloomPass.radius = 0.4
-unrealBloomPass.threshold = 0.85
-effectComposer.addPass(unrealBloomPass)
-
-// Custom Tint Pass
-const TintPass = {
-    uniforms: {
-        tDiffuse: { value: null },
-        uTint: { value: new THREE.Color('white') },
-    },
-    vertexShader: `
-        varying vec2 vUv;
-
-        void main() {
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-            vUv = uv;
-        }
-    `,
-    fragmentShader: `
-    uniform vec3 uTint;
-    uniform sampler2D tDiffuse;
-    varying vec2 vUv;
-
-    void main() {
-        vec4 texel = texture2D(tDiffuse, vUv);
-        gl_FragColor = vec4(texel.rgb * uTint, texel.a);
-    }
-    `
-}
-
-const tintPass = new ShaderPass(TintPass)
-effectComposer.addPass(tintPass)
-gui.add(tintPass.uniforms.uTint.value, 'r').min(0).max(1).step(0.001).name('tintR')
-gui.add(tintPass.uniforms.uTint.value, 'g').min(0).max(1).step(0.001).name('tintG')
-gui.add(tintPass.uniforms.uTint.value, 'b').min(0).max(1).step(0.001).name('tintB')
-
-// Disaplacement Pass
-const DisplacementPass = {
-    uniforms: {
-        tDiffuse: { value: null },
-        uTime: { value: 0 },
-        tDisplacement: { value: null },
-        uDisplacementScale: { value: 0.1 },
-        uDisplacementBias: { value: 0 },
-        uNormalMap: { value: null },
-    },
-    vertexShader: `
-        varying vec2 vUv;
-
-        void main() {
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-            vUv = uv;
-        }
-    `,
-    fragmentShader: `
-        uniform sampler2D tDiffuse;
-        uniform sampler2D tDisplacement;
-        uniform float uDisplacementScale;
-        uniform float uDisplacementBias;
-        uniform float uTime;
-        uniform sampler2D uNormalMap;
-
-        varying vec2 vUv;
-
-        void main() {
-            vec4 normalColor = texture2D(uNormalMap, vUv).xyzw * 2.0 - 1.0;
-            vec2 newUv = vUv + normalColor.xy * uDisplacementScale + uDisplacementBias;
-            vec4 color = texture2D(tDiffuse, newUv);
-
-            vec3 lightDirection = normalize(vec3(- 1.0, 1.0, 0.0));
-            vec3 normal = normalize(normalColor.xyz);
-            float lightness = clamp(dot(normal, lightDirection), 0.0, 1.0);
-            color.rgb += lightness * 2.0;
-            
-            gl_FragColor = color;
-        }
-    `
-}
-
-const displacementPass = new ShaderPass(DisplacementPass)
-displacementPass.uniforms.uNormalMap.value = textureLoader.load('/textures/interfaceNormalMap.png')
-effectComposer.addPass(displacementPass)
-gui.add(displacementPass.uniforms.uDisplacementScale, 'value').min(0).max(0.3).step(0.001).name('displacementScale')
-gui.add(displacementPass.uniforms.uDisplacementBias, 'value').min(0).max(0.3).step(0.001).name('displacementBias')
-
-const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader)
-// gammaCorrectionPass.enabled = false
-effectComposer.addPass(gammaCorrectionPass)
-
-if (renderer.getPixelRatio() === 1 && !renderer.capabilities.isWebGL2) {
-    const smaaPass = new SMAAPass(renderTarget.texture)
-    effectComposer.addPass(smaaPass)
-}
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 3, 2.25)
+scene.add(directionalLight)
 
 /**
  * Animate
  */
+
+// // Tip 1
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+    // stats.begin()
     const elapsedTime = clock.getElapsedTime()
+
+    // Update test mesh
+    torusKnot.rotation.y = elapsedTime * 0.1
 
     // Update controls
     controls.update()
 
     // Render
-    // renderer.render(scene, camera)
-    effectComposer.render()
+    renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+    stats.end()
 }
 
 tick()
+
+/**
+ * Tips
+ */
+// Tip 2
+
+/** Disable FPS limit
+There is a way to unlock Chrome frame rate regardless of the screen capabilities.
+
+That will enable frame rate monitoring even on good computers. For example, if you are developing on a good computer and you see 60fps, you might think it's okay. But maybe your website can only run at 70~80fps on that good computer, but the frame rate will drop below 60fps on other computers, and you won't know it.
+
+If you unlock the frame rate limit, you'll see that the performances aren't good enough, and you should run at something like 150~200fps on this computer to be safe.
+
+To unlock Chrome framerate:
+
+Close it completely —write the following instructions somewhere else if you are looking at this lesson on Chrome.
+Open the terminal.
+Open the following Github gist and launch the right command —Mac or Windows: https://gist.github.com/brunosimon/c15e7451a802fa8e34c0678620022f7d
+Chrome should open without the frame rate limit. You can test it on with the exercise by opening the FPS meter again. If it didn't work, close it and retry. If it still doesn't work, you'll have to do without it.
+
+Be careful; doing this will draw much more power from your computer and might result on Chrome crashing. **/
+
+// // Tip 3
+
+// spector.js is a library that allows you to inspect your scene in real time. It's a great tool to debug your scene and see what's going on.
+// reduce draw calls
+
+// // Tip 4
+// console.log(renderer.info)
+
+// Tip 5
+// optimize JS code
+
+// // Tip 6
+// scene.remove(cube)
+// cube.geometry.dispose()
+// cube.material.dispose()
+// console.log(renderer.info)
+
+// Tip 7
+// use less lights, if possible without lights
+
+// // Tip 8
+// Avoid removing and adding lights
+
+// // Tip 9
+// Avoid three.js shadows, bake them in your 3D software
+
+// // Tip 10
+// directionalLight.shadow.camera.top = 3
+// directionalLight.shadow.camera.right = 6
+// directionalLight.shadow.camera.left = - 6
+// directionalLight.shadow.camera.bottom = - 3
+// directionalLight.shadow.camera.far = 10
+// directionalLight.shadow.mapSize.set(1024, 1024)
+
+// const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(cameraHelper)
+
+// // Tip 11 
+// only cast and receive shadows on objects that need it
+// cube.castShadow = true
+// cube.receiveShadow = false
+
+// torusKnot.castShadow = true
+// torusKnot.receiveShadow = false
+
+// sphere.castShadow = true
+// sphere.receiveShadow = false
+
+// floor.castShadow = false
+// floor.receiveShadow = true
+
+// // Tip 12
+// renderer.shadowMap.autoUpdate = false
+// renderer.shadowMap.needsUpdate = true
+
+// // Tip 13
+// Use less textures, resize as snall as possible
+
+// // Tip 14
+// Keep a power of 2 for your textures
+// Use right format, PNG for transparency, JPG for no transparency
+
+// // Tip 15
+
+
+// // Tip 18
+// for(let i = 0; i < 50; i++)
+// {
+//     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+
+//     const material = new THREE.MeshNormalMaterial()
+    
+//     const mesh = new THREE.Mesh(geometry, material)
+//     mesh.position.x = (Math.random() - 0.5) * 10
+//     mesh.position.y = (Math.random() - 0.5) * 10
+//     mesh.position.z = (Math.random() - 0.5) * 10
+//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+
+//     scene.add(mesh)
+// }
+
+// Tip 19
+// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+// const material = new THREE.MeshNormalMaterial()
+
+// for(let i = 0; i < 50; i++)
+// {
+//     const mesh = new THREE.Mesh(geometry, material)
+//     mesh.position.x = (Math.random() - 0.5) * 10
+//     mesh.position.y = (Math.random() - 0.5) * 10
+//     mesh.position.z = (Math.random() - 0.5) * 10
+//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+
+//     scene.add(mesh)
+// }
+
+// // Tip 20 merge geometries with BufferGeometryUtils
+// const geometries = []
+
+// for(let i = 0; i < 50; i++)
+// {
+//     const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+//     geometry.rotateX((Math.random() - 0.5) * Math.PI * 2)
+//     geometry.rotateY((Math.random() - 0.5) * Math.PI * 2)
+//     geometry.translate(
+//         (Math.random() - 0.5) * 10,
+//         (Math.random() - 0.5) * 10,
+//         (Math.random() - 0.5) * 10
+//     )
+
+//     geometries.push(geometry)
+// }
+
+// const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries)
+// scene.add(new THREE.Mesh(mergedGeometry, new THREE.MeshNormalMaterial()))
+
+// Tip 22
+// const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5)
+
+// const material = new THREE.MeshNormalMaterial()
+// const mesh = new THREE.InstancedMesh(geometry, material, 50)
+// // if you're going to change the matrix every frame, you need to set the usage to dynamic
+// mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage)
+// scene.add(mesh)
+
+// for(let i = 0; i < 50; i++)
+// {
+//     const position = new THREE.Vector3(
+//         (Math.random() - 0.5) * 10,
+//         (Math.random() - 0.5) * 10,
+//         (Math.random() - 0.5) * 10
+//     )
+
+//     const quaternion = new THREE.Quaternion()
+//     quaternion.setFromEuler(new THREE.Euler(
+//         (Math.random() - 0.5) * Math.PI * 2,
+//         (Math.random() - 0.5) * Math.PI * 2,
+//         0))
+
+//     const matrix = new THREE.Matrix4()
+//     matrix.makeRotationFromQuaternion(quaternion)
+//     matrix.setPosition(position)
+//     mesh.setMatrixAt(i, matrix)
+// }
+
+// // Tip 29
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// // Tip 31, 32, 34 and 35
+// const shaderGeometry = new THREE.PlaneGeometry(10, 10, 256, 256)
+
+// const shaderMaterial = new THREE.ShaderMaterial({
+//     uniforms:
+//     {
+//         uDisplacementTexture: { value: displacementTexture },
+//         uDisplacementStrength: { value: 1.5 }
+//     },
+//     vertexShader: `
+//         uniform sampler2D uDisplacementTexture;
+//         uniform float uDisplacementStrength;
+
+//         varying vec2 vUv;
+
+//         void main()
+//         {
+//             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+//             float elevation = texture2D(uDisplacementTexture, uv).r;
+//             if(elevation < 0.5)
+//             {
+//                 elevation = 0.5;
+//             }
+
+//             modelPosition.y += elevation * uDisplacementStrength;
+
+//             gl_Position = projectionMatrix * viewMatrix * modelPosition;
+
+//             vUv = uv;
+//         }
+//     `,
+//     fragmentShader: `
+//         uniform sampler2D uDisplacementTexture;
+
+//         varying vec2 vUv;
+
+//         void main()
+//         {
+//             float elevation = texture2D(uDisplacementTexture, vUv).r;
+//             if(elevation < 0.25)
+//             {
+//                 elevation = 0.25;
+//             }
+
+//             vec3 depthColor = vec3(1.0, 0.1, 0.1);
+//             vec3 surfaceColor = vec3(0.1, 0.0, 0.5);
+//             vec3 finalColor = vec3(0.0);
+//             finalColor.r += depthColor.r + (surfaceColor.r - depthColor.r) * elevation;
+//             finalColor.g += depthColor.g + (surfaceColor.g - depthColor.g) * elevation;
+//             finalColor.b += depthColor.b + (surfaceColor.b - depthColor.b) * elevation;
+
+//             gl_FragColor = vec4(finalColor, 1.0);
+//         }
+//     `
+// })
+
+// const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial)
+// shaderMesh.rotation.x = - Math.PI * 0.5
+// scene.add(shaderMesh)
